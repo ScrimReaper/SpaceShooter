d4a7d37d9009d27fea99f1ad349cfc08f71c6277
ScrimReaper <166304133+ScrimReaper@users.noreply.github.com>
2025-12-01 17:45:45 +0100

    feat(enemy): refactor Enemy module and add hitbox integration
    - Added `HC` circle-based hitbox for Enemy objects.
    - Enhanced `Enemy` constructor to initialize position, size, speed, and image scaling.
    - Introduced `draw` and `update` methods for better modularity.
    - Utilized `utils.calcHitboxPos` for hitbox updates.

--- enemy.lua
+++ enemy.lua
@@ -1,23 +1,48 @@
+local utils = require("utils")
+
 local Enemy = {}
+Enemy.__index=Enemy
 Enemy.health = 100
 Enemy.width = 15
 Enemy.height = 15
 local enemy_speed = 200
 
-function Enemy.new(x)
-    return { x = x, y = 0, dead = false }
+function Enemy.new(x,y,image)
+    local self = setmetatable({}, Enemy)
+    self.image=image
+
+    self.x = x or 0
+    self.y = y or 0
+
+    self.w = 32
+    self.h = 24
+    self.speed = 200
+
+    self.scaleX = self.w / image:getWidth()
+    self.scaleY = self.h / image:getHeight()
+
+
+    -- hitbox
+    local cx = x + self.w/2
+    local cy = y + self.h/2
+    self.hitbox = HC.circle(cx, cy, 12)
+
+    return self
 end
 
-function Enemy.draw(e)
-    love.graphics.setColor(1, 0, 1)
-    love.graphics.rectangle("fill", e.x, e.y, Enemy.width, Enemy.height)
-    love.graphics.setColor(1, 1, 1)
+function Enemy:draw()
+    love.graphics.draw(self.image, self.x, self.y, 0, self.scaleX, self.scaleY)
 end
 
-function Enemy.update(e, dt)
-    jump = enemy_speed * dt
-    x_Move(e, jump)
-    y_Move(e, jump)
+function Enemy:update(dt)
+    self.y = self.y + self.speed * dt
+
+    local hx,hy = utils.calcHitboxPos(self)
+    self.hitbox:moveTo(hx,hy)
+end
+
+function Enemy:getY()
+    return self.y
 end
 
 function x_Move(e, jump)
